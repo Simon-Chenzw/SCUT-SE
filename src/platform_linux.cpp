a45fec4406e220646b6da0e0998fb9750dc99261
Simon_Chen <1020359403@qq.com>
2020-06-28 13:57:23 +0800

    add a "const"

--- src/platform_linux.cpp
+++ src/platform_linux.cpp
@@ -86,7 +86,7 @@ void Keyboard::clean_buffer() {
 }
 
 // color
-int clr_code[] = {0, 30, 31, 32, 33, 34, 35, 36, 37, 40, 41, 42, 43, 44, 45, 46, 47};
+const int clr_code[] = {0, 30, 31, 32, 33, 34, 35, 36, 37, 40, 41, 42, 43, 44, 45, 46, 47};
 
 void setcolor(const int& color) {
     if (color == CLR_RESET)
