fafa9f8e8c34c3264f75b00a43417d1a060179ed
Simon_Chen <1020359403@qq.com>
2021-11-08 12:46:54 +0800

    [exp-1] complete

--- scanner/src/main/Main.java
+++ scanner/src/main/Main.java
@@ -1,18 +1,20 @@
 package main;
 
 import java.io.IOException;
-import java.util.Hashtable;
 
 import lexer.Lexer;
 import lexer.Token;
 
+/// 程序函数入口
 public class Main {
 	public static void main(String[] args) throws IOException {
+		/// 使用 lexer 处理读入直到 '\n'
 		Lexer lexer = new Lexer();
-		char c;
+		// char c;
 		do {
 			Token token = lexer.scan();
 			switch (token.tag) {
+			/// 依据 tag 的值，识别当前的 token 的类型，依此输出类型
 			case 270:
 			case 272:
 				System.out.println("(NUM , " + token.toString() + ")");
@@ -32,6 +34,33 @@ public class Main {
 				break;
 			case 13:
 				break;
+			/** custom */
+			case 300:
+				System.out.println("(STR , " + token.toString() + ")");
+				break;
+			case 301:
+			case 302:
+			case 303:
+			case 304:
+			case 305:
+			case 306:
+			case 307:
+			case 308:
+			case 309:
+			case 310:
+			case 311:
+			case 312:
+			case 313:
+			case 314:
+			case 315:
+			case 316:
+			case 317:
+				System.out.println("(KEY , " + token.toString() + ")");
+				break;
+
+			case 400:
+				return;
+
 			default:
 				System.out.println("(SYM , " + token.toString() + ")");
 				break;
