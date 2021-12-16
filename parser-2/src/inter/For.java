edbc8ba3921e15dffd9c40f50e3ff6d120145767
Simon Chen <1020359403@qq.com>
2021-12-16 16:48:17 +0000

    complete for.java

--- parser-2/src/inter/For.java
+++ parser-2/src/inter/For.java
@@ -10,8 +10,35 @@ public class For extends Stmt {
 	public void init(Stmt s1, Expr x, Stmt s2, Stmt s3) {
 		expr = x; stmt1 = s1; stmt2 = s2; stmt3 = s3;
 		//此处添加对for语句的条件表达式的类型检查，有类型错误时提示for语句语义错误
+		if (expr.type != Type.Bool)
+			expr.error("boolean required in while");
+		// stmt1 - Set or SetElem
+		if (!(stmt1 instanceof Set || stmt1 instanceof SetElem))
+			stmt1.error("stmt1 must be a assign");
+		// stmt2 - Set or SetElem
+		if (!(stmt2 instanceof Set || stmt2 instanceof SetElem))
+			stmt2.error("stmt2 must be a assign");
 	}
-	
-	public void gen(int b, int a) {}
+
+	// 实验四 中间代码生成
+	public void gen(int b, int a) {
+		int init_lb = newlabel();
+		int expr_lb = newlabel();
+		int stmt_lb = newlabel();
+		int step_lb = newlabel();
+		// init
+		emitlabel(init_lb);
+		stmt1.gen(init_lb, expr_lb);
+		// check expr
+		emitlabel(expr_lb);
+		expr.jumping(0, a);
+		// stmt
+		emitlabel(stmt_lb);
+		stmt3.gen(stmt_lb, step_lb);
+		// step
+		emitlabel(step_lb);
+		stmt2.gen(step_lb, expr_lb);
+		emit("goto L" + expr_lb);
+	}
 
 }
