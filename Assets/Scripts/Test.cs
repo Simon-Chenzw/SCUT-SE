6735766a6323e093517c2427f0eafc6c2c210c12
Simon Chen <1020359403@qq.com>
2022-06-02 14:49:55 +0800

    setup code format check (#1)

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,13 +7,11 @@ public class Test : MonoBehaviour
     public float RotateSpeed = 0.0f;
 
     // Start is called before the first frame update
-    void Start()
-    {        
-    }
+    void Start() { }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0.0f, RotateSpeed * Time.deltaTime, 0.0f)); 
+        transform.Rotate(new Vector3(0.0f, RotateSpeed * Time.deltaTime, 0.0f));
     }
 }
