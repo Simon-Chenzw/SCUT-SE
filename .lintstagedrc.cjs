7ffdfd706ff7f34bd41f53c535196bc9caf56725
Simon_Chen <1020359403@qq.com>
2023-04-15 15:16:04 +0800

    ci: check code use lint & formatter

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,4 @@
 module.exports = {
-  "*.{js,ts,jsx,tsx}": ["yarn lint", "yarn format"],
-  "!*.{js,ts,jsx,tsx}": ["yarn format"],
+  "*.{js,ts,jsx,tsx}": ["yarn lint --fix", "yarn format --write"],
+  "!*.{js,ts,jsx,tsx}": ["yarn format --write"],
 }
