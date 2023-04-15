6c7c71ef61d2da7bbf41d78657779c57adce8fb0
Simon Chen <1020359403@qq.com>
2023-04-15 16:48:25 +0800

    feat: redirect to login page when unauthorized (#2)

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import type { NextRequest } from "next/server"
+import { NextResponse } from "next/server"
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+}
+
+export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/auth")) return
+
+  request.headers
+
+  const resp = await fetch(new URL("/api/auth/me", request.nextUrl.origin), {
+    headers: {
+      cookie: request.headers.get("cookie") || "",
+    },
+  })
+  const { code } = await resp.json()
+  if (code !== 0) return NextResponse.redirect(new URL("/", request.url))
+}
