ae684e66e78a6c1515ff0fcb32d899a129eca23b
Simon Chen <1020359403@qq.com>
2023-04-26 02:19:54 +0800

    chore: script to create request & result (#20)

--- script/create.mjs
+++ script/create.mjs
@@ -2,6 +2,37 @@ import { PrismaClient } from "../lib/prisma-client/index.js"
 
 const prisma = new PrismaClient({ log: ["query"] })
 
+/**
+ * @param {number} val
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+const clamp = (val, min, max) => (val > max ? max : val < min ? min : val)
+
+// return (0, 1)
+const randOpen = () => {
+  let r = 0
+  while (r === 0) r = Math.random()
+  return r
+}
+
+const gaussian = () =>
+  Math.sqrt(-2 * Math.log(randOpen())) * Math.cos(2 * Math.PI * randOpen())
+
+/**
+ * @param {[number, number][]} p
+ * @returns {(x:number)=>number}
+ */
+const lagrange = (p) => (x) =>
+  p.reduce(
+    (acc, [xi, yi], i) =>
+      acc +
+      (yi * p.reduce((acc, [xj], j) => (i !== j ? acc * (x - xj) : acc), 1)) /
+        p.reduce((acc, [xj], j) => (i !== j ? acc * (xi - xj) : acc), 1),
+    0
+  )
+
 export async function create_user(
   number = 10000,
   createStart = new Date("2023-01-01"),
@@ -37,9 +68,65 @@ export async function create_user(
   await Promise.all(task)
 }
 
+export async function create_result(
+  number = 1000,
+  createStart = new Date("2023-01-01"),
+  createEnd = new Date("2023-04-30"),
+  email = "User1@local"
+) {
+  await prisma.user.findUniqueOrThrow({ where: { email } })
+  await prisma.machinedResult.deleteMany({
+    where: { request: { user: { email } } },
+  })
+  await prisma.request.deleteMany({ where: { user: { email } } })
+
+  const getDate = (x) =>
+    new Date(createStart.getTime() * (1 - x) + createEnd.getTime() * x)
+
+  const basicGet =
+    (area, ...args) =>
+    (x) => ({
+      area,
+      score: clamp(
+        lagrange(args.map((v, i) => [i / (args.length - 1), v]))(x) +
+          gaussian() * 5,
+        0,
+        100
+      ),
+      region: { h: 0, w: 0, x: 0, y: 0 },
+      message: "",
+    })
+  const getHead = basicGet("head", 40, 55, 50, 40)
+  const getBody = basicGet("body", 50, 30, 60, 65)
+  const getLeg = basicGet("leg", 70, 35, 40, 50)
+
+  const task = new Array(number)
+    .fill(0)
+    .map((v, idx) => idx / number)
+    .map((x) =>
+      prisma.request.create({
+        data: {
+          createdAt: getDate(x),
+          user: { connect: { email } },
+          machinedResult: {
+            create: {
+              createdAt: getDate(x),
+              data: [getHead(x), getBody(x), getLeg(x)],
+            },
+          },
+        },
+      })
+    )
+
+  await Promise.all(task)
+}
+
 async function main() {
-  await create_user()
-  console.log(await prisma.user.count())
+  // await create_user()
+  // console.log(await prisma.user.count())
+  await create_result()
+  console.log(await prisma.request.count())
+  console.log(await prisma.machinedResult.count())
 }
 
 main()
